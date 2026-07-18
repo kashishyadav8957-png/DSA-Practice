a8c0b2a3819c84d394e39b4012549c218d556664
kashishyadav8957-png <kashishyadav8957@gmail.com>
2026-07-18 23:26:45 +0530

    Create  SortList.cpp

---
+++ TwoPointers/SortList.cpp
@@ -0,0 +1,47 @@
+
+class Solution {
+public:
+ 
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+
+    
+    ListNode* getMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+   
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* mid = getMid(head);
+        ListNode* right = mid->next;
+        mid->next = nullptr;
+
+        ListNode* leftSorted = sortList(head);
+        ListNode* rightSorted = sortList(right);
+
+        return merge(leftSorted, rightSorted);
+    }
+};

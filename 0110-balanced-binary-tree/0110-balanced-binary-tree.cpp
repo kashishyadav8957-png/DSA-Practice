09acfc649570352ad623fb34301b2d5e82c4d172
kashishyadav8957-png <kashishyadav8957@gmail.com>
2026-08-05 10:34:09 +0530

    0110-balanced-binary-tree .cpp

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int height(TreeNode *root){
+        if(root == NULL)
+            return 0;
+        int l = height(root -> left);
+        int r = height(root -> right);
+        return max(l,r)+1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) return true;
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        return abs(leftHeight - rightHeight) <= 1 
+            && isBalanced(root->left) 
+            && isBalanced(root->right);
+    }
+};

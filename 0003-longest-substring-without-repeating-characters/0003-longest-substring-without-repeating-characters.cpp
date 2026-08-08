083838f8bfc054a5cedcd9de2360b242b04e2283
kashishyadav8957-png <kashishyadav8957@gmail.com>
2026-08-08 23:35:30 +0530

    0003-longest-substring-without-repeating-characters .cpp

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> mp;
+        
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            
+            if (mp.find(s[right]) != mp.end()) {
+                left = max(left, mp[s[right]] + 1);
+            }
+
+            mp[s[right]] = right;
+
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};

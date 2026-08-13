46c5729e0d5e38a153fd0a509b5135fd54c348c5
kashishyadav8957-png <kashishyadav8957@gmail.com>
2026-08-13 23:33:57 +0530

    1652-defuse-the-bomb .cpp

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> result(n, 0);
+
+        if (k == 0) return result;
+
+        int sum = 0;
+        int start, end;
+
+        if (k > 0) {
+            start = 1;
+            end = k;
+        } else {
+            start = n + k;  
+            end = n - 1;
+            k = -k; 
+        }
+
+        for (int i = start; i <= end; i++) {
+            sum += code[i % n];
+        }
+
+     
+        for (int i = 0; i < n; i++) {
+            result[i] = sum;
+            sum -= code[(start + i) % n];
+            sum += code[(end + 1 + i) % n];
+        }
+
+        return result;
+    }
+};

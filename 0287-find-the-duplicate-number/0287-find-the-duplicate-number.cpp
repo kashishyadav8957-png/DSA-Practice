7de31efac146057b8e5e7eb83fd376b173396ff4
kashishyadav8957-png <kashishyadav8957@gmail.com>
2026-09-12 11:38:02 +0530

    0287-find-the-duplicate-number .cpp

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
+        int slow=0,fast=0;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>1){
-                return nums[i];
+            while(true){
+                slow=nums[slow];
+                fast=nums[fast];
+                fast=nums[fast];
+                if(slow==fast){
+                    slow=0;
+                    while(slow!=fast){
+                        slow=nums[slow];
+                        fast=nums[fast];
+                    }
+                
+                return slow;}
             }
-
         }
         return 0;
-
         
     }
 };
